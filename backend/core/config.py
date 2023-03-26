becdefdf9a637a174fdc069b21485bd59b473803
baabbii <yohanesalemu0069@gmail.com>
2023-03-26 17:04:59 +0300

    Database connected

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,6 +1,21 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+env_path = Path(".") / ".env"
+load_dotenv(dotenv_path=env_path)
+
+
 class Settings:
     PROJECT_TITLE: str = "Jobboard"
     PROJECT_VERSION: str = "0.1.0"
+
+    POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER")
+    POSTGRES_USER: str = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_DB: str = os.getenv("POSTGRES_DB")
+    POSTGRES_PORT: str = os.getenv("POSTGRES_PORT")
+    DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
 
 settings = Settings()
